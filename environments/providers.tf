b120ca730aa3e495e3751733990ec552a82dd754
Nagarajan Manokaran <lmnagaraj@gmail.com>
2021-08-31 15:37:26 +0800

    update providers

--- environments/providers.tf
+++ environments/providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   version                 = "~> 2.0"
   region                  = var.aws_region
   profile                 = "default"
-  shared_credentials_file = "/Users/mahaakutty/.aws/credentials_hashitalks"
+  shared_credentials_file = "home-dir/.aws/credentials_hashitalks"
 
   assume_role {
     role_arn = "arn:aws:iam::${var.aws_account_id}:role/administrator"
--- main/providers.tf
+++ main/providers.tf
@@ -2,6 +2,6 @@ provider "aws" {
   version                 = ">= 2.0"
   region                  = var.aws_region
   profile                 = "default"
-  shared_credentials_file = "/Users/mahaakutty/.aws/credentials_hashitalks"
+  shared_credentials_file = "<home-dir>.aws/credentials_hashitalks"
 
 }
